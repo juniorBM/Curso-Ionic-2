9a1e2ff95bda5b7942be84f7da67e1f21b318e07
Junior Mendonça <Junior Mendona>
2017-05-24 00:57:36 -0300

    Alterado o gitignore para poder commitar as pastas www dos exemplos

---
+++ Curso Ionic 2/Generator/www/build/2.main.js
@@ -0,0 +1,42 @@
+webpackJsonp([2],{
+
+/***/ 270:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(37);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__my_tabs__ = __webpack_require__(273);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MyTabsModule", function() { return MyTabsModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var MyTabsModule = (function () {
+    function MyTabsModule() {
+    }
+    return MyTabsModule;
+}());
+MyTabsModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__my_tabs__["a" /* MyTabs */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__my_tabs__["a" /* MyTabs */]),
+        ]
+    })
+], MyTabsModule);
+
+//# sourceMappingURL=my-tabs.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=2.main.js.map
